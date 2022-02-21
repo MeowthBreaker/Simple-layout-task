e8a742f398910b17556f048ffe8ad6a0bfb7e87e
Bugama <bugama227@gmail.com>
2022-02-21 22:00:30 +0300

    our services

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 const arrow = document.querySelector(".header__anchor");
 const about = document.querySelector(".about");
+const serviceCards = [...document.querySelectorAll(".services__card")];
 
 arrow.addEventListener("click", scrollToAbout);
+
+serviceCards.forEach(card => {
+    let a = card.querySelector(".services__card-button");
+    let toggle = () => {
+        a.classList.toggle('button_theme_transparent-onhover');
+    }
+    card.addEventListener("mouseenter", toggle);
+    card.addEventListener("mouseleave", toggle);
+});
 
 function scrollToAbout() {
     about.scrollIntoView({behavior: "smooth", block: "center"});
