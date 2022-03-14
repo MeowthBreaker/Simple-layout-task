8dc9e0c121996d0e275e57edac4bd7aec70ae990
Bugama <bugama227@gmail.com>
2022-03-14 11:53:17 +0300

    team

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const arrow = document.querySelector(".header__anchor");
 const about = document.querySelector(".about");
 const serviceCards = [...document.querySelectorAll(".services__card")];
+const pricingCards = [...document.querySelectorAll(".pricing-plan__card")];
 
 arrow.addEventListener("click", scrollToAbout);
 
@@ -8,6 +9,15 @@ serviceCards.forEach(card => {
     let a = card.querySelector(".services__card-button");
     let toggle = () => {
         a.classList.toggle('button_theme_transparent-onhover');
+    }
+    card.addEventListener("mouseenter", toggle);
+    card.addEventListener("mouseleave", toggle);
+});
+
+pricingCards.forEach(card => {
+    let a = card.querySelector(".pricing-plan__card-button");
+    let toggle = () => {
+        a.classList.toggle('button_theme_green-onhover');
     }
     card.addEventListener("mouseenter", toggle);
     card.addEventListener("mouseleave", toggle);
